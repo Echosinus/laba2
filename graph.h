f6f580276fbe569c590570cd6be089b8c18867de
Echosinus <61726989+Echosinus@users.noreply.github.com>
2020-09-09 07:34:05 +0300

    Update graph.h

--- graph.h
+++ graph.h
@@ -1,12 +1,13 @@
 #pragma once
-#define N 100
-struct queue //очередь
+#define N 99
+struct queue //РѕС‡РµСЂРµРґСЊ
 {
 	int que[N];
 	int quefirst, quelast;
 };
-void inqueue(struct queue* q); //инициализация очереди
-int isempty(struct queue* q); //проверка пуста ли очередь
-void print(struct queue* q); //вывод элементов
-void push(struct queue* q, int x); //добавление в очередь
-int pop(struct queue* q); // удаление в очереди
+void init_queue(struct queue* q); //РёРЅРёС†РёР°Р»РёР·Р°С†РёСЏ РѕС‡РµСЂРµРґРё
+int isempty(struct queue* q); //РїСЂРѕРІРµСЂРєР° РїСѓСЃС‚Р° Р»Рё РѕС‡РµСЂРµРґСЊ
+int print(struct queue* q); //РІС‹РІРѕРґ СЌР»РµРјРµРЅС‚РѕРІ
+int push_node(struct queue* q, int x); //РґРѕР±Р°РІР»РµРЅРёРµ РІ РѕС‡РµСЂРµРґСЊ
+int graph_bypass(struct queue* q);//РѕР±С…РѕРґ РіСЂР°С„Р°
+int adding_element(struct queue* q); //РѕР±С…РѕРґ РіСЂР°С„Р°
