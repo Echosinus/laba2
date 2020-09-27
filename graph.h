cb8f3901ee4903ac45358abf1b524561280fd90b
Echosinus <61726989+Echosinus@users.noreply.github.com>
2020-09-27 19:39:18 +0300

    Update graph.h

--- graph.h
+++ graph.h
@@ -10,4 +10,3 @@ int isempty(struct queue* q); //проверка пуста ли очередь
 int print(struct queue* q); //вывод элементов
 int push_node(struct queue* q, int x); //добавление в очередь
 int graph_bypass(struct queue* q);//обход графа
-int adding_element(struct queue* q); //обход графа
